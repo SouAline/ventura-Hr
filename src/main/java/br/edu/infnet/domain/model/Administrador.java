3b61c8678ebeb8187e62d44babf429d360e6be61
Aline Souza <ds.alinesouza@gmail.com>
2021-08-21 13:34:20 -0300

    faz import, cria contrutor

--- src/main/java/br/edu/infnet/domain/model/Administrador.java
+++ src/main/java/br/edu/infnet/domain/model/Administrador.java
@@ -2,14 +2,50 @@ package br.edu.infnet.domain.model;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Entity;
 import java.time.LocalDate;
 
+@Entity
 public class Administrador extends Usuario{
 
     private String nome;
-    
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate dataNascimento;
     private String venturaId;
+    private String cpf;
+
+    public Administrador(){super();}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getVenturaId() {
+        return venturaId;
+    }
+
+    public void setVenturaId(String venturaId) {
+        this.venturaId = venturaId;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
